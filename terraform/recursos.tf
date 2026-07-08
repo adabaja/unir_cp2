b59edab34f2087f1cc000e57208d0a0da8fc9b43
Ahmed Dabaja Gebara <dabajaahmed@yahoo.com>
2026-07-08 10:20:09 -0500

    Evidencias (parte 1) + imagen app-web

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -12,3 +12,127 @@ resource "azurerm_container_registry" "acr" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+# Red virtual
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.prefix}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subred
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-vm"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# IP pública fija
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-${var.prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Firewall de red: solo SSH (administración) y HTTP/HTTPS (la web) desde Internet
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${var.prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Tarjeta de red de la VM
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${var.prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+
+resource "azurerm_network_interface_security_group_association" "nsg_nic" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vm-${var.prefix}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = "Standard_B2als_v2"
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  # Acceso solo por clave SSH (sin contraseñas)
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = var.prefix
+  }
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,9 +1,20 @@
 variable "location" {
   type    = string
-  default = "centralus"  # swedencentral bloqueada
+  default = "northcentralus"  # swedencentral bloqueada
 }
 
 variable "prefix" {
   type    = string
   default = "casopractico2"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+
+variable "ssh_public_key_path" {
+  type    = string
+  default = "~/.ssh/cp2_vm.pub"
 }
